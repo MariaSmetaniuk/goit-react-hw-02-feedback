5d9ab7c6e20ab454bc08a17496c7caefab4e5918
MariaSmetaniuk <smetaniukmaria@gmail.com>
2022-09-24 18:56:07 +0300

    text-shadow

--- src/components/Notification/Notification.styled.js
+++ src/components/Notification/Notification.styled.js
@@ -4,6 +4,7 @@ export const NotificationStyled = styled.p`
   font-size: 20px;
   font-weight: ${p => p.theme.fontWeights.bold};
   letter-spacing: 0.05rem;
-  text-shadow: #4b4b4b 1px 0 10px;
+  text-shadow: 2px 7px 5px rgba(0, 0, 0, 0.3),
+    0px -4px 10px rgba(255, 255, 255, 0.3);
   color: ${p => p.theme.colors.accent};
 `;
